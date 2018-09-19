34d1c3740c0928619f942ff59fd98b0514ffdfb1
Anton Korzunov <thekashey@gmail.com>
2018-09-20 08:59:02 +1000

    add onUpdate

--- src/component.tsx
+++ src/component.tsx
@@ -1,12 +1,17 @@
 import {PureComponent} from 'react';
 
 interface IProps {
-  gen?: any
+  gen?: any;
+  onUpdate?: () => void
 }
 
 export class EventHorizon extends PureComponent<IProps> {
   shouldComponentUpdate(nextProps: IProps) {
     return this.props.gen !== nextProps.gen;
+  }
+
+  componentDidUpdate() {
+    this.props.onUpdate && this.props.onUpdate();
   }
 
   render() {
